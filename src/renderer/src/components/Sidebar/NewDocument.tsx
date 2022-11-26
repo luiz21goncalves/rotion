b764adc3f5c24a31fd13c19395c9d7a7c60edcd8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-11-26 12:36:46 -0300

    feat(renderer): create new document

--- src/renderer/src/components/Sidebar/NewDocument.tsx
+++ src/renderer/src/components/Sidebar/NewDocument.tsx
@@ -1,8 +1,45 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Plus } from 'phosphor-react'
 
+import { QUERY_KEYS } from '@/shared/constants/query-keys'
+import type { Document } from '@/shared/types/ipc'
+
 export function NewDocument() {
+  const queryClient = useQueryClient()
+
+  const { mutateAsync: createDocument, isLoading: isCreatingNewDocument } =
+    useMutation(
+      async () => {
+        const response = await window.api.createDocument()
+
+        return response.data
+      },
+      {
+        onSuccess: (data) => {
+          queryClient.setQueryData<Document[]>(
+            [QUERY_KEYS.DOCUMENTS.FETCH_ALL],
+            (documents) => {
+              if (documents && documents?.length >= 0) {
+                return [...documents, data]
+              }
+
+              return [data]
+            },
+          )
+        },
+      },
+    )
+
+  function handleCreateDocument() {
+    createDocument()
+  }
+
   return (
-    <button className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60">
+    <button
+      disabled={isCreatingNewDocument}
+      onClick={handleCreateDocument}
+      className="flex w-[240px] px-5 items-center text-sm gap-2 absolute bottom-0 left-0 right-0 py-4 border-t border-rotion-600 hover:bg-rotion-700 disabled:opacity-60"
+    >
       <Plus className="h-4 w-4" />
       Novo documento
     </button>
